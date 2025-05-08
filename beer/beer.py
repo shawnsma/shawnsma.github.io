f605c96ad3d1e55cf28e5a78af415152c68193f4
shawnsma <shenglongma09@gmail.com>
2025-05-08 22:40:24 +0800

    map

--- beer/beer.py
+++ beer/beer.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
 
 breweries = [
-    {"name": "Tree House", "lat": 42.13679801905403, "long": -72.0123901407236},
-    {"name": "Trillium", "lat": 42.352513670121645, "long": -71.04749252578482},
-    {"name": "Yards", "lat": 39.96118386082349, "long": -75.14695123676813},
-    {"name": "Equilibrium", "lat": 41.444799039800664, "long": -74.41979249157365},
-    {"name": "Suarez Family", "lat": 42.111148113326124, "long": -73.81232254551149},
-    {"name": "Hudson Valley", "lat": 41.501638564463704, "long": -73.9630373897188}
+    {"name": "Tree House", "lat": 42.13679801905403, "lng": -72.0123901407236},
+    {"name": "Trillium", "lat": 42.352513670121645, "lng": -71.04749252578482},
+    {"name": "Yards", "lat": 39.96118386082349, "lng": -75.14695123676813},
+    {"name": "Equilibrium", "lat": 41.444799039800664, "lng": -74.41979249157365},
+    {"name": "Suarez Family", "lat": 42.111148113326124, "lng": -73.81232254551149},
+    {"name": "Hudson Valley", "lat": 41.501638564463704, "lng": -73.9630373897188}
 ]
 
 @app.route("/api/breweries")
