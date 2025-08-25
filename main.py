f1ceca238bb3dfe258084adb84f7538e9267b018
shawnsma <shenglongma09@gmail.com>
2025-08-25 09:43:04 -0400

    updated permissions

--- main.py
+++ main.py
@@ -29,6 +29,18 @@ RAW_EVENT_MAP_LOAD = "brewery_map_load"
 STIGG_URL = "https://api.stigg.io/graphql"
 HEADERS = {"X-API-KEY": API_KEY, "Content-Type": "application/json"} # header format
 
+BREWERIES = [
+    {"name": "Tree House", "lat": 42.13679801905403, "lng": -72.0123901407236},
+    {"name": "Trillium", "lat": 42.352513670121645, "lng": -71.04749252578482},
+    {"name": "Yards", "lat": 39.96118386082349, "lng": -75.14695123676813},
+    {"name": "Equilibrium", "lat": 41.444799039800664, "lng": -74.41979249157365},
+    {"name": "Suarez Family Brewery", "lat": 42.111148113326124, "lng": -73.81232254551149},
+    {"name": "Hudson Valley", "lat": 41.501638564463704, "lng": -73.9630373897188},
+    {"name": "Hill Farmstead", "lat": 44.60681234591264, "lng": -72.26338064539726},
+    {"name": "Plan Bee Farm Brewery", "lat": 41.719265242807616, "lng": -73.88649581669273},
+    {"name": "Other Half", "lat": 40.716382728061404, "lng": -73.96668371061196},
+]
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware, # def
@@ -217,3 +229,7 @@ async def report_map_load(payload: dict = Body(...)):
     }
     data = await gql(query, variables)
     return {"ok": True, "id": data.get("reportUsage", {}).get("id")}
+
+@app.get("/api/breweries")
+def list_breweries():
+    return BREWERIES
